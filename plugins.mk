fde1f1d4f3c91ad07bd79ae34194dee3dba82d2c
sadko4u <sadko4u@gmail.com>
2022-01-16 01:42:39 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-loud-comp.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_LOUD_COMP
-ARTIFACT_NAME               = lsp-plugins-loud-comp
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Loudness Compensator Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_LOUD_COMP
+PLUGIN_PACKAGE_NAME     = lsp-plugins
