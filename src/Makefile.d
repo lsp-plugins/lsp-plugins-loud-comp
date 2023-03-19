bba1cec16608222d328ba42604b7349072ccdf08
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:20:58 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -411,6 +411,7 @@ $(LSP_PLUGINS_LOUD_COMP_BIN)/main/ui/loud_comp.o: main/ui/loud_comp.cpp \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
